0ebc7c812bd19c344f0f649c22d40634cfd7063d
vladdoroniuk <vlad.doroniuk.dev@gmail.com>
2023-10-08 17:03:40 +0300

    chore: make .gitattributes ignore non-related files recursively

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,7 +9,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", fileServer)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
